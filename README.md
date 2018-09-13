c6ee7c5b2fd2afdbb776bdba0d21d4e782162bc4
VincentGrice <39641488+vgrice888@users.noreply.github.com>
2018-09-13 16:40:28 -0400

    adding port

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ Final project for Udemy course
 1. Clone this repo
 2. Run `npm install`
 3. Run `npm start`
-
--- controllers/signin.js
+++ controllers/signin.js
@@ -23,4 +23,4 @@ const handleSignin = (db, bcrypt) => (req, res) => {
 
 module.exports = {
   handleSignin: handleSignin
-}
+};
